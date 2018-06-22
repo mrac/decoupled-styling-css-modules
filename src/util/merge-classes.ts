fca0d2da4eff30a155ca4f1313cf85928e51ec8d
Michal <michal.raczkowski@zalando.de>
2018-06-22 16:51:34 +0200

    fix link

--- src/util/merge-classes.ts
+++ src/util/merge-classes.ts
@@ -1,5 +1,13 @@
 import { mergeDeep } from './merge-deep';
 
+const NESTED_DELIMITER = /__/;
+
+export function mergeClasses<T>(target: T, source?: T): T {
+  target = nested(target);
+  source = nested(source);
+  return mergeDeep(target, source);
+}
+
 /**
  * Translates CSS-Modules generated flat CSS-classes object, into nested object.
  *
@@ -25,15 +33,6 @@ import { mergeDeep } from './merge-deep';
  *  }
  *
  */
-
-const NESTED_DELIMITER = /__/;
-
-export function mergeClasses<T>(target: T, source?: T): T {
-  target = nested(target);
-  source = nested(source);
-  return mergeDeep(target, source);
-}
-
 function nested<T>(classes: T): T {
   const newClasses: T = {} as T;
 
